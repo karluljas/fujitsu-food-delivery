3159cf85acf992721f136a344d864cc9afba7d0a
karluljas <92479285+karluljas@users.noreply.github.com>
2025-03-22 16:07:35 +0200

    Initial project setup with Spring Boot

---
+++ fujitsu-food-delivery/src/main/java/com/fujitsu/fujitsu_food_delivery/FujitsuFoodDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.fujitsu.fujitsu_food_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FujitsuFoodDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FujitsuFoodDeliveryApplication.class, args);
+	}
+
+}
